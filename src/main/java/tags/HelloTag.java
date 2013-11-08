1f127cc1c8bc21657e0156e340e7c52630d5f626
ankru <anna.v.kruglova@gmail.com>
2013-11-08 18:13:40 +0400

    initial source codes add

---
+++ src/main/java/tags/HelloTag.java
@@ -0,0 +1,14 @@
+package tags;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+
+public class HelloTag extends SimpleTagSupport {
+
+  public void doTag() throws JspException, IOException {
+    JspWriter out = getJspContext().getOut();
+    out.println("Hello Custom Tag!");
+  }
+}
